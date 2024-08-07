f9aa996e3e62f434288adbb50becf1902d0b7173
Opeyemi ogungbe <136735745+opeyemiogungbe@users.noreply.github.com>
2024-08-07 16:20:28 -0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,9 @@
-# This file configures the rules for ESLint, specifying what should be checked.
-# Example .eslintrc content:
- {
-  "env": {
-    "browser": true,
-    "es6": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
+// eslint.config.js
+export default [
+    {
+        rules: {
+            semi: "error",
+            "prefer-const": "error"
+        }
     }
-  },
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "single"],
-    "indent": ["error", 2]
-  },
-  "plugins": [
-    "react"
-  ]
-}
+];
